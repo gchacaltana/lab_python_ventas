29737bd606a2fbfb75bc8f4db06bcaef916236df
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2022-09-25 10:42:47 -0500

    add config file

---
+++ config.py
@@ -0,0 +1,12 @@
+QUANTITY_MIN:int = 1
+QUANTITY_MAX:int = 3
+
+PRODUCT_MIN:int = 1
+PRODUCT_MAX:int = 3
+
+IGV_PERCENT = 18
+
+# 01 punto equivale a 33 
+VALUE_POINT = 33
+
+CURRENCY_SYMBOL = "S/. "
