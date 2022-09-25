8e8536fac61ff0d2c389c4e3280c223fa32cf59a
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2022-09-25 10:43:33 -0500

    update application - create sales tickets

--- application.py
+++ application.py
@@ -1,12 +1,16 @@
 from typing import List, Dict
+from config import QUANTITY_MIN, QUANTITY_MAX, PRODUCT_MAX, PRODUCT_MIN, IGV_PERCENT, VALUE_POINT, CURRENCY_SYMBOL
 import random
+import math
 
 # link typing: https://docs.python.org/3/library/typing.html?highlight=typing#module-typing
 # link random: https://docs.python.org/3/library/random.html
 
 """
-Implementación del proyecto de clase python: Ventas de una tienda de productos de tecnología.
+Implementación del proyecto de clase python: 
+Ventas de una tienda de productos de tecnología.
 """
+
 
 def main():
     """
@@ -14,7 +18,7 @@ def main():
     """
 
     # Lista de tiendas.
-    stores: List[Dict[str, str]] = [
+    stores: List[Dict[str, str | int | float]] = [
         {
             "code": "MIR",
             "name": "Miraflores",
@@ -52,7 +56,7 @@ def main():
         }
     ]
 
-    products: List[Dict[str, str]] = [
+    products: List[Dict[str, str | float | int]] = [
         {
             "name": "Memoria USB Kingston 64 GB",
             "base_sale_price": 38.90,
@@ -104,16 +108,25 @@ def main():
             "percent_discount": 5
         }
     ]
-    
+
     # iteramos sobre la cantidad de tiendas
     for key, store in enumerate(stores):
 
         # Obtenemos el total de ventas obtenidas de manera aleatoria
         # según los datos de cada tienda.
-        sales:int = random.randint(store["min_sales"],store["max_sales"])
+        sales: int = random.randint(
+            int(store["min_sales"]), int(store["max_sales"]))
 
-        # iteramos sobre la cantidad de ventas
+        # declarar e inicializar la lista que almacenará los tickets de venta.
+        list_tickets: List[Dict[str, str|float]] = []
+
+        # declarar e inicializar la lista que almacenará los detalles tickets de venta.
+        list_detail: List[Dict[str, str|float]] = []
+
+        # iteramos sobre la cantidad de ventas para crear los tickets de venta
         for i in range(sales):
+            # Crear los tickets de venta (aleatorio)
+
             # Número de Ticket Objetivo: "MIR00001"
             correlative: str = str(i+1).zfill(5)
 
@@ -123,14 +136,72 @@ def main():
             # random.sample => devuelve lista de números únicos aleatorios.
             # link: https://docs.python.org/3/library/random.html?highlight=random%20sample#random.sample
             # Los números aleatorios obtenidos serán los indices para obtener los productos.
-            list_rand_products = random.sample(range(len(products)), random.randint(1,3))
+            list_rand_products = random.sample(
+                range(len(products)), random.randint(PRODUCT_MIN, PRODUCT_MAX))
             # Ejemplo: [2,5,7]
 
-            # iteramos sobre la lista aleatoria de productos para crear el detalle del ticket
-            for k,v in enumerate(list_rand_products):
-                print(products[v]['name'])
+            # variable para almacenar el importe del subtotal del ticket de venta
+            ticket_subtotal: float = 0
 
+            # variable para almacenar el importe del total de descuentos.
+            ticket_discount: float = 0
+
+            # iteramos sobre la lista aleatoria de productos para crear el detalle del ticket
+            for k, v in enumerate(list_rand_products):
+
+                # calculando el precio de venta del producto según el porcentaje BSP de la tienda
+                price_sale: float = round(float(products[v]['base_sale_price']) + (
+                    float(products[v]['base_sale_price']) * float(store['percent_bsp'])/100), 2)
+
+                # calculando el importe del descuento del producto.
+                discount: float = round(
+                    price_sale * (float(products[v]['percent_discount'])/100), 2)
+
+                ticket_discount += discount
+
+                # calculando el precio final de venta del producto
+                final_sale_price: float = round(
+                    price_sale - abs(discount), 2)
+
+                quantity: int = random.randint(QUANTITY_MIN, QUANTITY_MAX)
+
+                total: float = round(final_sale_price*quantity, 2)
+
+                ticket_subtotal += total
+
+                detail: Dict[str, str | float] = {
+                    "ticket_number": ticket_number,
+                    "product_name": products[v]['name'],
+                    "base_sale_price": float(products[v]['base_sale_price']),
+                    "sale_price": price_sale,
+                    "discount": discount,
+                    "final_sale_price": final_sale_price,
+                    "quantity": quantity,
+                    "total": total
+                }
+
+                list_detail.append(detail)
+
+            ticket_igv: float = ticket_subtotal*IGV_PERCENT/100
+            ticket_total:float = ticket_subtotal + ticket_igv
+            ticket_points:int = math.ceil(ticket_subtotal/VALUE_POINT)
+
+            # Creamos el diccionario para el ticket de venta
+            ticket:Dict[str, str|float] = {
+                "number": ticket_number,
+                "subtotal": ticket_subtotal,
+                "igv": ticket_igv,
+                "total": ticket_total,
+                "points": ticket_points,
+                "discount": ticket_discount
+            }
+
+            list_tickets.append(ticket)
+
+        print(f"Tienda: {store['name']}")
+        print(f"Total de Tickets: {len(list_tickets)}")
         #print(f"{store['name']} Total de Tickets: {sales}")
+
 
 if __name__ == "__main__":
     main()
